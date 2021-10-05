e811772523f558c6b41953da2503c3d7b8f89e62
kyokuheishin <kyokuheishin@gmail.com>
2021-10-06 00:18:22 +0900

    Add regex matcher

--- dsmcc/context_manager.py
+++ dsmcc/context_manager.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractclassmethod, abstractmethod
 from util import singleton
+import re
 
 
 class NameSpaceState:
@@ -44,6 +45,64 @@ class NameSpaceState:
     @property
     def base_uri(self):
         return self._base_uri
+
+    def get_absolute_path(self, arg: str):
+        if not arg or arg == "":
+            return None
+
+        path = arg.strip()
+        path = re.sub(r'^~/', "/"+self._current_component_tag+"/", path)
+
+        #case: "/<component>/<module>"
+        m = re.match(r"^/([0-9A-Fa-f]{2})/([0-9A-Fa-f]{4})/?$", path)
+        if m:
+            self._current_component_tag = m.group(1)
+            self._current_module_id = m.group(2)
+            self._current_resource_name = None
+            return f"/{self._current_component_tag}/{self._current_module_id}"
+
+        #case: "/<component>/<module>/<resource>"
+        m = re.match(r"^/([0-9A-Fa-f]{2})/([0-9A-Fa-f]{4})/(.+)$", path)
+        if m:
+            self._current_component_tag = m.group(1)
+            self._current_module_id = m.group(2)
+            self._current_resource_name = m.group(3)
+            return f"/{self._current_component_tag}/{self._current_module_id}/{self._current_resource_name}"
+
+        #case: "<module>"
+        m = re.match(r"^([0-9A-Fa-f]{4})$", path)
+        if m:
+            self._current_module_id = m.group(1)
+            self._current_resource_name = None
+            return f"/{self._current_component_tag}/{self._current_module_id}"
+
+        #case: "/<module>/<resource>"
+        m = re.match(r"^/?([0-9A-Fa-f]{4})/(.+)$", path)
+        if m:
+            self._current_module_id = m.group(1)
+            self._current_resource_name = m.group(2)
+            return f"/{self._current_component_tag}/{self._current_module_id}/{self._current_resource_name}"
+
+        #case: "<resource>"
+        m = re.match(r"^([^/]+)$", path)
+        if m:
+            self._current_resource_name = m.group(1)
+            return f"/{self._current_component_tag}/{self._current_module_id}/{self._current_resource_name}"
+
+        #case: "aribdc"
+        m = re.match(
+            r"^arib-dc://-1.-1.-1/([0-9A-Fa-f]{2})/([0-9A-Fa-f]{4})/(.*)$", path)
+        if m:
+            self._current_component_tag = m.group(1)
+            self._current_module_id = m.group(2)
+            self._current_resource_name = m.group(3)
+            return f"/{self._current_component_tag}/{self._current_module_id}/{self._current_resource_name}"
+        return None
+
+    def update_current_state(self, path: str):
+        self.get_absolute_path(path)
+        self._base_uri = None
+        self._current_uri = None
 
 
 @singleton
